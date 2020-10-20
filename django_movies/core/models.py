286647c11d5390314954406f6d3d1fbfd77915eb
Broks-Komp <skorbrejs@gmail.com>
2020-10-20 18:29:54 +0200

    commit: dodanie w settings "core", urls "sciezki", views "def", stworzenie modelu w models

--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    rating = models.ImageField(
+        null=True, validators=[MaxLengthValidator(10), MinLengthValidator(1)]
+    )
+    released = models.DateField()
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} from {self.released}"
 
 # Create your models here.
