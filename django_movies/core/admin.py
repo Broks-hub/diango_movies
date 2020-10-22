bc86422eb253dbe9fb3b3b43daa3aa08fbb90136
Broks-Komp <skorbrejs@gmail.com>
2020-10-22 19:25:27 +0200

    git commit -m "Add Director and Genre model"

--- django_movies/core/admin.py
+++ django_movies/core/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from core.models import Movie
 from core.models import Genre
+from core.models import Director
 
 admin.site.register(Movie)
 admin.site.register(Genre)
+admin.site.register(Director)
--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -4,10 +4,17 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    age_limit = models.IntegerField(max_length=3, null=True)
+    age_limit = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
+
+
+class Director(models.Model):
+    full_name = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.full_name
 
 
 class Movie(models.Model):
@@ -19,6 +26,7 @@ class Movie(models.Model):
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     genre = models.ForeignKey(Genre, null=True, on_delete=models.DO_NOTHING)
+    director = models.ForeignKey(Director, null=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f"{self.title} from {self.released}"
