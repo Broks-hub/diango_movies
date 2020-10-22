17f6127ca29455e15d52e1bf80fc98bdc0f509d4
Broks-Komp <skorbrejs@gmail.com>
2020-10-22 18:13:41 +0200

    commit: 3 test

--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -1,18 +1,27 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.core.validators import MaxLengthValidator, MinLengthValidator
-from django.db import models
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    age_limit = models.IntegerField(max_length=3, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    rating = models.ImageField(
-        null=True, validators=[MaxLengthValidator(10), MinLengthValidator(1)]
+    rating = models.IntegerField(
+        null=True, validators=[MaxValueValidator(10), MinValueValidator(1)]
     )
     released = models.DateField()
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f"{self.title} from {self.released}"
 
-# Create your models here.
+
+
