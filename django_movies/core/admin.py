89835988f78b2f57d19d1d8c9c8fd199ec92ce7e
Broks-Komp <skorbrejs@gmail.com>
2020-10-26 17:38:06 +0100

    git commit -m "Przed zajeciami, zmiany w admin, fixtures, models, db.sqlite"

--- django_movies/core/admin.py
+++ django_movies/core/admin.py
@@ -4,6 +4,10 @@ from core.models import Movie
 from core.models import Genre
 from core.models import Director
 
+from core.models import Country
+
 admin.site.register(Movie)
 admin.site.register(Genre)
 admin.site.register(Director)
+admin.site.register(Country)
+
--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -2,6 +2,13 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
+class Country(models.Model):
+    country_name = models.CharField(max_length=50, null=True, unique=True)
+
+    def __str__(self):
+        return self.country_name
+
+
 class Genre(models.Model):
     name = models.CharField(max_length=20, unique=True)
     age_limit = models.IntegerField(null=True)
@@ -26,10 +33,14 @@ class Movie(models.Model):
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     genre = models.ForeignKey(Genre, null=True, on_delete=models.DO_NOTHING)
-    director = models.ForeignKey(Director, null=True, on_delete=models.DO_NOTHING)
+    director = models.ForeignKey(Director, null=True, on_delete=models.SET_NULL)
+    countries = models.ManyToManyField(Country, related_name='movies')
 
     def __str__(self):
         return f"{self.title} from {self.released}"
+
+    class Meta:
+        unique_together = ('title', 'released')
 
 
 
